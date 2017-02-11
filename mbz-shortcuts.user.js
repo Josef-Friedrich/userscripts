c804e3d99ada4bc9cf45100d2bb1b8011a69212e
Josef Friedrich <josef@friedrich.rocks>
2017-02-11 08:59:39 +0100

    Add test function

--- mbz-shortcuts.user.js
+++ mbz-shortcuts.user.js
@@ -4,18 +4,20 @@
 // @name         MusicBrainz: Shortcuts
 // @namespace    mbz-joseffriedrich
 // @author       Josef Friedrich
-// @version      2017.1.22
-// @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-showacoustid.user.js
-// @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-showacoustid.user.js
-// @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
-// @icon         https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/icon.png
-// @description  musicbrainz.org: Show acoustids on a Work page
+// @version      2017.02.11
+// @downloadURL  https://raw.githubusercontent.com/Josef-Friedrich/greasemonkey-user-scripts/master/mbz-shortcuts.user.js
+// @updateURL    https://raw.githubusercontent.com/Josef-Friedrich/greasemonkey-user-scripts/master/mbz-shortcuts.user.js
+// @supportURL   https://github.com/Josef-Friedrich/greasemonkey-user-scripts
+// @icon         https://raw.githubusercontent.com/Josef-Friedrich/greasemonkey-user-scripts/master/icon.png
+// @description  musicbrainz.org: Add some shortcuts
 // @compatible   firefox+greasemonkey
-// @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
-// @require      https://greasyfork.org/scripts/13747-mbz-loujine-common/code/mbz-loujine-common.js?version=128923
-// @include      http*://*musicbrainz.org/work/*
-// @include      http*://*musicbrainz.org/artist/*/relationships
-// @exclude      http*://*musicbrainz.org/work/*/*
+// @licence      MIT (https://opensource.org/licenses/MIT)
+// @include      http*://musicbrainz.org/release/*/edit-relationships
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
+
+$(document).ready(function() {
+    $('.rel-add').click(function() {alert("Add!");});
+    return false;
+});
