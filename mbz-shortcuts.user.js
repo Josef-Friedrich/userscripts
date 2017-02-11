c6a2f033cc378087bececf9d76be1ae4bb5a17a4
Josef Friedrich <josef@friedrich.rocks>
2017-02-11 08:47:23 +0100

    Add user script

---
+++ mbz-shortcuts.user.js
@@ -0,0 +1,21 @@
+/* global $ _ requests sidebar */
+'use strict';
+// ==UserScript==
+// @name         MusicBrainz: Shortcuts
+// @namespace    mbz-joseffriedrich
+// @author       Josef Friedrich
+// @version      2017.1.22
+// @downloadURL  https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-showacoustid.user.js
+// @updateURL    https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/mbz-showacoustid.user.js
+// @supportURL   https://bitbucket.org/loujine/musicbrainz-scripts
+// @icon         https://bitbucket.org/loujine/musicbrainz-scripts/raw/default/icon.png
+// @description  musicbrainz.org: Show acoustids on a Work page
+// @compatible   firefox+greasemonkey
+// @licence      CC BY-NC-SA 3.0 (https://creativecommons.org/licenses/by-nc-sa/3.0/)
+// @require      https://greasyfork.org/scripts/13747-mbz-loujine-common/code/mbz-loujine-common.js?version=128923
+// @include      http*://*musicbrainz.org/work/*
+// @include      http*://*musicbrainz.org/artist/*/relationships
+// @exclude      http*://*musicbrainz.org/work/*/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
