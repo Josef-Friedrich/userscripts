99108d1af74ce34b73b86802039ddcb5006ac407
Josef Friedrich <josef@friedrich.rocks>
2017-02-11 10:50:04 +0100

    Add accesskey attributes

--- mbz-shortcuts.user.js
+++ mbz-shortcuts.user.js
@@ -4,7 +4,7 @@
 // @name         MusicBrainz: Shortcuts
 // @namespace    mbz-joseffriedrich
 // @author       Josef Friedrich
-// @version      2017.02.11
+// @version      1
 // @downloadURL  https://raw.githubusercontent.com/Josef-Friedrich/greasemonkey-user-scripts/master/mbz-shortcuts.user.js
 // @updateURL    https://raw.githubusercontent.com/Josef-Friedrich/greasemonkey-user-scripts/master/mbz-shortcuts.user.js
 // @supportURL   https://github.com/Josef-Friedrich/greasemonkey-user-scripts
@@ -19,8 +19,6 @@
 // ==/UserScript==
 
 $(document).ready(function() {
-    $('.rel-add').click(function() {alert("Add!");});
-    Mousetrap.bind('4', function() { alert("Add!"); });
-    Mousetrap.bind('ctrl+1', function() { alert("controll!"); });
-    Mousetrap.bind('ctrl+2', function() { openBatchRecordingsDialog(); });
+    document.getElementById("batch-recording").setAttribute("accesskey", "r");
+    document.getElementById("batch-work").setAttribute("accesskey", "w");
 });
