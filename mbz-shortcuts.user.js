1c7a071cf5eee25a8427bec732e30870ae32fba5
Josef Friedrich <josef@friedrich.rocks>
2017-02-12 08:41:32 +0100

    hotkey version

--- mbz-shortcuts.user.js
+++ mbz-shortcuts.user.js
@@ -13,22 +13,21 @@
 // @compatible   firefox+greasemonkey
 // @licence      MIT (https://opensource.org/licenses/MIT)
 // @include      http*://musicbrainz.org/release/*/edit-relationships
-// @require      https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.min.js
+// @require      https://raw.githubusercontent.com/jeresig/jquery.hotkeys/master/jquery.hotkeys.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
 function AddShortCuts(id, key) {
-    var link = $("#" + id);
-    link.attr({
-        title: "Shortcut: " + key,
-        accesskey: key
-    });
-    var text = link.html();
-    link.html(text + " (" + key + ")");
+  var link = $('#' + id);
+  link.bind('keydown', key, function() { this.click();
+  });
+  link.attr({
+    title: 'Shortcut: ' + key
+  });
+  var text = link.html();
+  link.html(text + ' (' + key + ')');
 }
-
-
-AddShortCuts("batch-recording", "r");
-AddShortCuts("batch-create-works", "n");
-AddShortCuts("batch-work", "w");
+AddShortCuts('batch-recording', 'r');
+AddShortCuts('batch-create-works', 'n');
+AddShortCuts('batch-work', 'w');
